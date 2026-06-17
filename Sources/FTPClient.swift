fc10a965cb5d5c187abefe35ed23f43e6095c15d
samjan190799-cmyk <samjan190799@gmail.com>
2026-06-18 00:29:29 +0400

    fix: change readLine to read byte-by-byte to prevent consuming TLS handshake bytes

--- Sources/FTPClient.swift
+++ Sources/FTPClient.swift
@@ -27,7 +27,7 @@ class FTPClient {
                         }
                         
                         let chunk = try await withCheckedThrowingContinuation { (continuation: CheckedContinuation<Data, Error>) in
-                            self.connection.receive(minimumIncompleteLength: 1, maximumLength: 4096) { data, _, _, error in
+                            self.connection.receive(minimumIncompleteLength: 1, maximumLength: 1) { data, _, _, error in
                                 if let error = error {
                                     continuation.resume(throwing: error)
                                 } else if let data = data {
